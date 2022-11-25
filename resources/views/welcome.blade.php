bbbb130a3d94ecf6791dabd60868b2b3221d7ff9
juan.tabares <juan.tabares@coytex.com.co>
2022-11-24 22:55:46 -0500

    livewire installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        @livewireStyles
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -128,5 +129,6 @@
                 </div>
             </div>
         </div>
+        @livewireScripts
     </body>
 </html>
