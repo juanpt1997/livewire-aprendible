0668dee2cde61117d74dfa935f747bb8d8fd676b
steven <gironsteven75@gmail.com>
2022-12-09 22:25:17 -0500

    probando alternativas que tienen los inputs de livewire

--- app/Http/Livewire/Articles.php
+++ app/Http/Livewire/Articles.php
@@ -7,6 +7,10 @@ use Livewire\Component;
 
 class Articles extends Component
 {
+    public array $tags = [];
+    public array $category = ['name' => ''];
+    // public $search;
+
     public function render()
     {
         return view('livewire.articles', ['articles' => Article::all()]);
--- resources/views/livewire/articles.blade.php
+++ resources/views/livewire/articles.blade.php
@@ -2,6 +2,25 @@
     {{-- The best athlete wants his opponent at his best. --}}
     <h1>Listado de artículos</h1>
 
+    <h2>Tags: @json($tags)</h2>
+    <div>
+        <label for="">
+            Tag 1
+        </label>
+        <input wire:model="tags" type="checkbox" value="tag1">
+    </div>
+    <div>
+        <label for="">
+            Tag 2
+        </label>
+        <input wire:model="tags" type="checkbox" value="tag2">
+    </div>
+    <h2>Category: @json($category)</h2>
+    <input type="text" wire:model="category.name">
+
+
+    {{-- <input type="text" wire:model="category"> --}}
+
     <ul>
         @foreach ($articles as $article)
             <li>{{ $article->title }}</li>
