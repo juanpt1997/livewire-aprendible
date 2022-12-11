fc70ad97f643e2fd8c0cab90b76441c2cc00a48a
steven <gironsteven75@gmail.com>
2022-12-11 13:02:04 -0500

    validate fields with livewire

--- app/Http/Livewire/ArticleForm.php
+++ app/Http/Livewire/ArticleForm.php
@@ -10,12 +10,35 @@ class ArticleForm extends Component
     public $title;
     public $content;
 
+    protected $rules = ['title' => 'required|min:4',
+                        'content' => 'required'];
+
+    // ? These custom messages should be written directly on translation files
+    // protected $messages = [
+    //     'title.required' => 'El título (:attribute) es obligatorio'
+    // ];
+    // protected $validationAttributes = [
+    //     'title' => 'Título'
+    // ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName); // We only want to validate one specific field
+    }
+
     public function save()
     {
-        $article = new Article;
-        $article->title = $this->title;
-        $article->content = $this->content;
-        $article->save();
+        // $data = $this->validate([
+        //     'title' => 'required',
+        //     'content' => 'required'
+        // ]);
+        $data = $this->validate();
+        // As it returns an array only with validated data, we can do something different than below
+        // $article = new Article;
+        // $article->title = $this->title;
+        // $article->content = $this->content;
+        // $article->save();
+        Article::create($data);
 
         // $this->reset();
         session()->flash('status', __('Article created.'));
--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -5,11 +5,17 @@
         <div>
             <label for=""></label>
             <input wire:model='title' type="text" placeholder="Título">
+            @error('title')
+                <div>{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for=""></label>
             <textarea wire:model='content' placeholder="Contenido" id="" cols="30" rows="10"></textarea>
+            @error('content')
+                <div>{{ $message }}</div>
+            @enderror
         </div>
 
         <input type="submit" value="Guardar">
