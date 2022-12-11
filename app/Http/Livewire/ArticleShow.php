e3704310c61acda0c06bbe7efead522984247471
steven <gironsteven75@gmail.com>
2022-12-11 14:57:36 -0500

    typed properties (model as property) replacing mount method to still use route model binding

--- app/Http/Livewire/ArticleShow.php
+++ app/Http/Livewire/ArticleShow.php
@@ -7,14 +7,17 @@ use Livewire\Component;
 
 class ArticleShow extends Component
 {
-    public $article;
 
-    public function mount(Article $article)
-    {
-        // $this->article = Article::find(request()->route('article'));
-        // $this->article = Article::findOrFail($article); // if it is just an id
-        $this->article = $article;
-    }
+    // ? We can avoid the mount method with Typed Properties (models as properties), instead of writing this code:
+    // public $article;
+    // public function mount(Article $article)
+    // {
+    //     // $this->article = Article::find(request()->route('article'));
+    //     // $this->article = Article::findOrFail($article); // if it is just an id
+    //     $this->article = $article;
+    // }
+    // ? We can just write this and everything works fine, automatically applies route model binding:
+    public Article $article;
 
     public function render()
     {
